20da3567c011a290771f97653b252048c8ccbaa8
Amakhin <amakhinruslan@gmail.com>
2019-11-25 00:32:03 +0200

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Amakhin Ruslan on 25.11.2019.
+//  Copyright © 2019 Amakhin Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
